3205373afa039b64471b764f31262a10ee4aed6f
swarage <sdcoolio@gmail.com>
2014-08-20 13:15:20 -0400

    fixed images

--- index.php
+++ index.php
@@ -107,21 +107,21 @@
             <div class="row">
                 <div class="4u">
                     <article class="box box-style2">
-                        <a href="http://flypixel.com/generic-smartphone/8949517882265310" class="image image-full"><img src="images/portfolio01.jpg" alt="" /></a>
+                        <a href="http://flypixel.com/generic-smartphone/8949517882265310" class="image image-full"><img src="images/hacking.gif" alt="" /></a>
                         <h3><a href="http://flypixel.com/generic-smartphone/8949517882265310">1st place codebusters</a></h3>
                         <p>Science Olympiad Virginia States</p>
                     </article>
                 </div>
                 <div class="4u">
                     <article class="box box-style2">
-                        <a href="http://flypixel.com/n33" class="image image-full"><img src="images/portfolio02.jpg" alt="" /></a>
+                        <a href="http://flypixel.com/n33" class="image image-full"><img src="images/earth.png" alt="" /></a>
                         <h3><a href="http://flypixel.com/n33">3rd place Dynamic Planet</a></h3>
                         <p>Lake Braddock Regionals</p>
                     </article>
                 </div>
                 <div class="4u">
                     <article class="box box-style2">
-                        <a href="http://flypixel.com/wood-ui-kit/3574765984616310" class="image image-full"><img src="images/portfolio03.jpg" alt="" /></a>
+                        <a href="http://flypixel.com/wood-ui-kit/3574765984616310" class="image image-full"><img src="images/rube.jpg" alt="" /></a>
                         <h3><a href="http://flypixel.com/wood-ui-kit/3574765984616310">4th place Mission Possible</a></h3>
                         <p>Lake Braddock Regionals</p>
                     </article>
@@ -130,21 +130,21 @@
             <div class="row">
                 <div class="4u">
                     <article class="box box-style2">
-                        <a href="http://flypixel.com/n33-pattern-set-1/3522389001865317" class="image image-full"><img src="images/portfolio04.jpg" alt="" /></a>
+                        <a href="http://flypixel.com/n33-pattern-set-1/3522389001865317" class="image image-full"><img src="images/sapphire_1x.jpg" alt="" /></a>
                         <h3><a href="http://flypixel.com/n33-pattern-set-1/3522389001865317">4th place Rocks and Minerals</a></h3>
                         <p>Lake Braddock Regionals</p>
                     </article>
                 </div>
                 <div class="4u">
                     <article class="box box-style2">
-                        <a href="http://flypixel.com/cityscape/9803996277226316" class="image image-full"><img src="images/portfolio05.jpg" alt="" /></a>
+                        <a href="http://flypixel.com/cityscape/9803996277226316" class="image image-full"><img src="images/widi.jpg" alt="" /></a>
                         <h3><a href="http://flypixel.com/cityscape/9803996277226316">5th place Write it Do it</a></h3>
                         <p>Lake Braddock Regionals</p>
                     </article>
                 </div>
                 <div class="4u">
                     <article class="box box-style2">
-                        <a href="http://flypixel.com/n33" class="image image-full"><img src="images/portfolio06.jpg" alt="" /></a>
+                        <a href="http://flypixel.com/n33" class="image image-full"><img src="images/flask_1x.png" alt="" /></a>
                         <h3><a href="http://flypixel.com/n33">5th place Experimental Design</a></h3>
                         <p>Lake Braddock Regionals</p>
                     </article>
